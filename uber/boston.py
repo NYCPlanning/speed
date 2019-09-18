2ad30cf76fdf13f4eb654507946dbd75edd2e1f6
mayijun1203 <mayijun1203@gmail.com>
2019-09-17 22:02:59 -0400

    Update boston.py

--- uber/boston.py
+++ uber/boston.py
@@ -5,6 +5,7 @@ import numpy as np
 path='D:/BOSTON RD/TRANSPORTATION/MAP/UBER/'
 
 osm=gpd.read_file(path+'osm.geojson')
+osm.crs={'init':'epsg:4326'}
 
 osmseg=pd.read_csv(path+'movement-segments-to-osm-ways-new-york-2018.csv',dtype=str,converters={'osm_way_id':float})
 osmseg['segment_id']=[' '.join(x.split()) for x in osmseg['segment_id']]
@@ -23,7 +24,7 @@ q1=pd.merge(q1,osmseg,how='left',left_on='segment_id',right_on='segment_id')
 q1=pd.merge(q1,osmnode,how='left',left_on='start_junction_id',right_on='junction_id')
 q1=pd.merge(q1,osmnode,how='left',left_on='end_junction_id',right_on='junction_id')
 q1=q1[['osm_way_id','osm_node_id_x','osm_node_id_y','speed_mph_mean','speed_mph_p50']].reset_index(drop=True)
-q1.columns=['osmwayid','osmstartnode','osmendnode','speedmeanq1','speedmedianq1']
+q1.columns=['wayid','startnode','endnode','meanq1','medianq1']
 
 q2=pd.read_csv(path+'movement-speeds-quarterly-by-hod-new-york-2018-Q2.csv',dtype=float,converters={'segment_id':str,
                                                                                                     'start_junction_id':str,
@@ -36,7 +37,7 @@ q2=pd.merge(q2,osmseg,how='left',left_on='segment_id',right_on='segment_id')
 q2=pd.merge(q2,osmnode,how='left',left_on='start_junction_id',right_on='junction_id')
 q2=pd.merge(q2,osmnode,how='left',left_on='end_junction_id',right_on='junction_id')
 q2=q2[['osm_way_id','osm_node_id_x','osm_node_id_y','speed_mph_mean','speed_mph_p50']].reset_index(drop=True)
-q2.columns=['osmwayid','osmstartnode','osmendnode','speedmeanq2','speedmedianq2']
+q2.columns=['wayid','startnode','endnode','meanq2','medianq2']
 
 q3=pd.read_csv(path+'movement-speeds-quarterly-by-hod-new-york-2018-Q3.csv',dtype=float,converters={'segment_id':str,
                                                                                                     'start_junction_id':str,
@@ -49,7 +50,7 @@ q3=pd.merge(q3,osmseg,how='left',left_on='segment_id',right_on='segment_id')
 q3=pd.merge(q3,osmnode,how='left',left_on='start_junction_id',right_on='junction_id')
 q3=pd.merge(q3,osmnode,how='left',left_on='end_junction_id',right_on='junction_id')
 q3=q3[['osm_way_id','osm_node_id_x','osm_node_id_y','speed_mph_mean','speed_mph_p50']].reset_index(drop=True)
-q3.columns=['osmwayid','osmstartnode','osmendnode','speedmeanq3','speedmedianq3']
+q3.columns=['wayid','startnode','endnode','meanq3','medianq3']
 
 q4=pd.read_csv(path+'movement-speeds-quarterly-by-hod-new-york-2018-Q4.csv',dtype=float,converters={'segment_id':str,
                                                                                                     'start_junction_id':str,
@@ -62,15 +63,15 @@ q4=pd.merge(q4,osmseg,how='left',left_on='segment_id',right_on='segment_id')
 q4=pd.merge(q4,osmnode,how='left',left_on='start_junction_id',right_on='junction_id')
 q4=pd.merge(q4,osmnode,how='left',left_on='end_junction_id',right_on='junction_id')
 q4=q4[['osm_way_id','osm_node_id_x','osm_node_id_y','speed_mph_mean','speed_mph_p50']].reset_index(drop=True)
-q4.columns=['osmwayid','osmstartnode','osmendnode','speedmeanq4','speedmedianq4']
+q4.columns=['wayid','startnode','endnode','meanq4','medianq4']
 
-uber=pd.merge(osm,q1,how='left',on=['osmwayid','osmstartnode','osmendnode'])
-uber=pd.merge(uber,q2,how='left',on=['osmwayid','osmstartnode','osmendnode'])
-uber=pd.merge(uber,q3,how='left',on=['osmwayid','osmstartnode','osmendnode'])
-uber=pd.merge(uber,q4,how='left',on=['osmwayid','osmstartnode','osmendnode'])
-uber['speedmean2018']=np.nanmean(uber[['speedmeanq1','speedmeanq2','speedmeanq3','speedmeanq4']],axis=1)
-uber['speedmedian2018']=np.nanmedian(uber[['speedmedianq1','speedmedianq2','speedmedianq3','speedmedianq4']],axis=1)
-uber=uber[['osmwayid','osmstartnode','osmendnode','osmname','osmhighway','osmoneway','speedmeanq1','speedmeanq2','speedmeanq3',
-           'speedmeanq4','speedmean2018','speedmedianq1','speedmedianq2','speedmedianq3','speedmedianq4','speedmedian2018','geometry']]
+uber=pd.merge(osm,q1,how='left',on=['wayid','startnode','endnode'])
+uber=pd.merge(uber,q2,how='left',on=['wayid','startnode','endnode'])
+uber=pd.merge(uber,q3,how='left',on=['wayid','startnode','endnode'])
+uber=pd.merge(uber,q4,how='left',on=['wayid','startnode','endnode'])
+uber['mean2018']=np.nanmean(uber[['meanq1','meanq2','meanq3','meanq4']],axis=1)
+uber['median2018']=np.nanmedian(uber[['medianq1','medianq2','medianq3','medianq4']],axis=1)
+uber=uber[['wayid','startnode','endnode','name','highway','oneway','meanq1','meanq2','meanq3','meanq4','mean2018','medianq1','medianq2',
+           'medianq3','medianq4','median2018','geometry']]
 uber.to_file(path+'boston.shp')
 
